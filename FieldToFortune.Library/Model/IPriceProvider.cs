401cf1d3012de92bcbe3bfbf5d4cb28a39a9018d
Naomie <naomie.kouassi@grenoble-inp.org>
2026-04-15 15:26:02 +0200

    Initial empty branch for deployment

--- FieldToFortune.Library/Model/IPriceProvider.cs
+++
@@ -1,9 +0,0 @@
-namespace FieldToFortune.Model;
-
-public interface IPriceProvider
-{
-    public double GetPrice(string commodityName, int turn);
-    public double[] GetFirstPrices(string commodityName, int lastTurn);
-    public Dictionary<string, double[]> GetAllPrices();
-    public Dictionary<string, double> GetAllPrices(int turn);
-}
